68592ad307cf3997a798baa69584b8b97f09497f
Sergey Bulychev <pds7@ukr.net>
2020-04-14 20:02:04 +0300

    module-01 correct 30%

--- module-01/js/task-02.js
+++ module-01/js/task-02.js
@@ -4,7 +4,7 @@ const total = 100;
 
 const ordered = 80;
 
-if (total <= ordered) {
+if (total < ordered) {
   console.log('На складе недостаточно товаров!');
 } else if (total >= ordered) {
   console.log('Заказ оформлен, с вами свяжется менеджер');

c1f54f4b5ebf92ea11898de8067fb61c1947a644
Sergey Bulychev <pds7@ukr.net>
2020-05-20 22:43:33 +0300

    module-03 80%

--- module_03/js/task-05.js
+++ module_03/js/task-05.js
@@ -1,1 +1,26 @@
 'use strict';
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  let elementsArray = [];
+  for (const object of arr) {
+    if (object.hasOwnProperty(prop)) {
+      elementsArray.push(object[prop]);
+    }
+  }
+  return elementsArray;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []

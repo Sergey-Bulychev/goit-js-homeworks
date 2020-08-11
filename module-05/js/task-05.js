2c3032d7f35c8b56b30dd91a718b9c09cc7f7b22
Sergey Bulychev <pds7@ukr.net>
2020-08-11 09:45:30 +0300

    module_5  correct 70%

--- module-05/js/task-05.js
+++ module-05/js/task-05.js
@@ -1,12 +1,12 @@
 'use strict';
 class Car {
-  /*
+	/*
    * Добавь статический метод `getSpecs(car)`,
    * который принимает объект-машину как параметр и выводит
    * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
    */
 
-  /*
+	/*
    * Конструктор получает объект настроек.
    *
    * Добавь свойства будущеего экземпляра класса:
@@ -16,42 +16,79 @@ class Car {
    *  isOn - заведен ли автомобиль, значения true или false. Изначально false
    *  distance - общий киллометраж, изначально 0
    */
-  constructor() {}
-
-  /*
+	constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
+		this._speed = speed;
+		this._price = price;
+		this._maxSpeed = maxSpeed;
+		this._isOn = isOn;
+		this._distance = distance;
+	}
+	static getSpecs(car) {
+		const dateCar = {
+			speed: car._speed,
+			maxSpeed: car._maxSpeed,
+			isOn: car._isOn,
+			distance: car._distance,
+			price: car._price
+		};
+		console.table(dateCar);
+	}
+	/*
    * Добавь геттер и сеттер для свойства price,
    * который будет работать с свойством цены автомобиля.
    */
+	get price() {
+		return this._price; 
+	}
+	set price(value) {
+		this._price = value; /
+	}
 
-  /*
-   * Добавь код для того чтобы завести автомобиль
-   * Записывает в свойство isOn значение true
-   */
-  turnOn() {}
+	/*
+	* Добавь код для того чтобы завести автомобиль
+	* Записывает в свойство isOn значение true
+	*/
 
-  /*
-   * Добавь код для того чтобы заглушить автомобиль
-   * Записывает в свойство isOn значение false,
-   * и сбрасывает текущую скорость в 0
-   */
-  turnOff() {}
+	turnOn() {
+		if (!this._isOn) this._isOn = true; 
+  }
+	/*
+	* Добавь код для того чтобы заглушить автомобиль
+	* Записывает в свойство isOn значение false,
+	* и сбрасывает текущую скорость в 0
+	*/
+	turnOff() {
+		if (this._isOn) this._isOn = false; 
+	}
 
-  /*
-   * Добавялет к свойству speed полученное значение,
-   * при условии что результирующая скорость
-   * не больше чем значение свойства maxSpeed
-   */
-  accelerate(value) {}
+	/*
+	* Добавялет к свойству speed полученное значение,
+	* при условии что результирующая скорость
+	* не больше чем значение свойства maxSpeed
+	*/
+	accelerate(value) {
+		if (value <= this._maxSpeed) this._speed += value; 
+	}
+	accelerate1(value) {
+		if (value <= this._maxSpeed) {
+			this._speed += value;
+		} else {
+			this._speed = this._maxSpeed; 
+	}
+	/*
+	* Отнимает от свойства speed полученное значение,
+	* при условии что результирующая скорость не меньше нуля
+	*/
+	decelerate(value) {
+		if (this._speed - value > -1) this._speed -= value; 
+	}
 
-  /*
-   * Отнимает от свойства speed полученное значение,
-   * при условии что результирующая скорость не меньше нуля
-   */
-  decelerate(value) {}
-
-  /*
-   * Добавляет в поле distance киллометраж (hours * speed),
-   * но только в том случае если машина заведена!
-   */
-  drive(hours) {}
+	/*
+	* Добавляет в поле distance киллометраж (hours * speed),
+	* но только в том случае если машина заведена!
+	*/
+	drive(hours) {
+		if (this._isOn) this._distance = hours *= this._speed; 
+	}
+}
 }
---
+++ module-06/js/task-01.js
@@ -0,0 +1,3 @@
+'use strict';
+//Получить массив имен всех пользователей (поле name).
+import users from './users.js';

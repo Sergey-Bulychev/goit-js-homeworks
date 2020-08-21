ec57d9c7952914eee6a5ba292fd5df5abba2778b
Sergey Bulychev <pds7@ukr.net>
2020-08-21 20:47:22 +0300

    correct module-06 60%

---
+++ module-06/js/task-10.js
@@ -0,0 +1,8 @@
+'use strict';
+import users from './users.js';
+const getSortedUniqueSkills = users =>
+  users
+    .map(user => user.skills)
+    .reduce((accum, skill) => [...accum, ...skill], [])
+    .sort((a, b) => a.localeCompare(b, 'en'))
+    .filter((skill, i, array) => array.indexOf(skill) === i);
